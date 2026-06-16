8d78ab66496c477b5d238b0e19d061a8a412bd9d
PredigtMiniatur <christianpieritz@yahoo.de>
2026-06-16 17:25:52 +0200

    Update daten.js

--- daten.js
+++ daten.js
@@ -4,5 +4,7 @@ export const smsglaubeInhalt = {
     initiale: "W",
     text1: "Das ist die erste Zeile",
     bibelvers: "»Görlitz",
-    text2: "Fließtext...."
+    text2: "Der Kirchengemeinderat der Ev. Kirchengemeinde ... Der Kirchengemeinderat der Ev. Kirchengemeinde ... Der Kirchengemeinderat der Ev. Kirchengemeinde ... Der Kirchengemeinderat der Ev. Kirchengemeinde ... Der Kirchengemeinderat der Ev. Kirchengemeinde ... Der Kirchengemeinderat der Ev. Kirchengemeinde ... Der Kirchengemeinderat der Ev. Kirchengemeinde ... Der Kirchengemeinderat der Ev. Kirchengemeinde ... 
+        
+        Der Kirchengemeinderat der Ev. Kirchengemeinde ... Der Kirchengemeinderat der Ev. Kirchengemeinde ... "
 };
