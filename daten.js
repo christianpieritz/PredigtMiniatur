e94905983c6997b0b91c1bf74273092c56f6716d
PredigtMiniatur <christianpieritz@yahoo.de>
2026-06-16 16:52:01 +0200

    Add files via upload

---
+++ daten.js
@@ -0,0 +1,8 @@
+// daten.js
+export const smsglaubeInhalt = {
+    ueberschrift: "DA IST EINER GEBOREN",
+    initiale: "D",
+    text1: "as ist Gott. Mitten in unserer Welt, unerwartet und leise. Er wird Mensch, um uns auf Augenhöhe zu begegnen, unsere Sorgen zu teilen und uns Licht in der Dunkelheit zu bringen.",
+    bibelvers: "»Denn uns ist ein Kind geboren, ein Sohn ist uns gegeben...« (Jesaja 9,5)",
+    text2: "Ein Versprechen, das bis heute trägt. Er ist da – für dich, für mich, für uns alle."
+};
