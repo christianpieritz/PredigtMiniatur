b36980b0dc325177d3b151f8489ba48c6486f8ac
PredigtMiniatur <christianpieritz@yahoo.de>
2026-06-17 07:29:41 +0200

    aktualisierung

--- daten.js
+++ daten.js
@@ -4,7 +4,12 @@ export const smsglaubeInhalt = {
     initiale: "W",
     text1: "Das ist die erste Zeile",
     bibelvers: "»Görlitz",
-    // HIER am Ende der Zeile wurde das Komma ergänzt:
-    text2: "Der Kirchengemeinderat der Ev. Kirchengemeinde ... Der Kirchengemeinderat der Ev. Kirchengemeinde ... Der Kirchengemeinderat der Ev. Kirchengemeinde ... Der Kirchengemeinderat der Ev. Kirchengemeinde ... Der Kirchengemeinderat der Ev. Kirchengemeinde ... Der Kirchengemeinderat der Ev. Kirchengemeinde ... Der Kirchengemeinderat der Ev. Kirchengemeinde ... Der Kirchengemeinderat der Ev. Kirchengemeinde ...",
-    text3: "Amen"
+    text2: "Der Kirchengemeinderat der Ev. Kirchengemeinde ...",
+    text3: "Amen",
+
+    // NEU: Die Daten für den nächsten Gottesdienst
+    gdTermin: "2026-06-21",      /* Datum im Format: JAHR-MONAT-TAG */
+    gdUhrzeit: "10:00",          /* Startzeit */
+    gdOrt: "Kirche Heringsdorf", /* Wo findet es statt? */
+    gdThema: "Gottesdienst mit Abendmahl" /* Titel des Kalendereintrags */
 };
