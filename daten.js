fdf6b1bff1f9cf53ff6331bf640d43db30b9be52
PredigtMiniatur <christianpieritz@yahoo.de>
2026-06-17 08:02:41 +0200

    Update daten.js

--- daten.js
+++ daten.js
@@ -2,14 +2,14 @@
 export const smsglaubeInhalt = {
     ueberschrift: "SMS·Glaube",
     initiale: "W",
-    text1: "Das ist die erste Zeile",
-    bibelvers: "»Görlitz",
-    text2: "Der Kirchengemeinderat der Ev. Kirchengemeinde ...",
+    text1: "Ein Satz aus der Predigt",
+    bibelvers: "»Ein zentraler Vers",
+    text2: "Hier der Kerngedanke des Sonntags.",
     text3: "Amen",
 
     // NEU: Die Daten für den nächsten Gottesdienst
     gdTermin: "2026-06-21",      /* Datum im Format: JAHR-MONAT-TAG */
     gdUhrzeit: "11:00", 
     gdOrt: "Kirche Heringsdorf",  
-    gdThema: "Gottesdienst mit Abendmahl"  
+    gdThema: "Gottesdienst"  
 };
